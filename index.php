38ddc4bf644767e4c987b2ce4662ff96b3328520
riza <rizaarifh338@gmail.com>
2019-09-13 05:11:21 +0700

    webbbb awal

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="src/css/bootstrap.css">
+    <link rel="stylesheet" href="src/css/my.css">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="https://kit.fontawesome.com/e05b7ef8c6.js"></script>
+    <title>Document</title>
+    
+</head>
+<body>
+    
+    <div class="d-block a" style="height:100vh;">
+    <div class="d-block" style="height:100px;">
+    <!-- <div style="width:100vw;"> -->
+    <!-- <div style="height:10vh; background:green;"> -->
+    
+        
+    
+
+    <!-- </div> -->
+    </div>
+    <div class="contentt">
+    <div class="bg-light" style="color:dark;height:100px;border: 2px solid white;border-radius: 40px 40px 0 0;width:42%;padding-left:20px;">
+    <div class="align-self-center">
+    <h1>Hello,There!</h1>
+    
+    <h2>I'm Riza Arif Hernawan</h2>
+
+    
+        
+    
+    </div>
+    </div>
+    
+    <a  href=""class="d-block bg-light" style="text-align:center; line-height:100px;color:dark;height:100px;border-radius: 0 0 0 40px;width:14%;padding-left:20px;float:left;">
+    Home</a>
+    <a href="" class="d-block bg-light" style="text-align:center; line-height:100px;color:dark;height:100px;border-radius: 0 0 0 0;width:14%;padding-left:20px;float:left;">
+    About</a>    
+<a href="" class="d-block bg-light" style="text-align:center; line-height:100px;color:dark;height:100px;border-radius: 0 0 40px 0;width:14%;padding-left:20px;float:left;">
+    Contact</a>    
+</div>    
+</div>
+</div>
+    </div>
+    </div>
+</body>
+</html>
